e524f7be7b62a1ba656f30783facb4a25abb0174
Katarzyna Jablonska <kaska.j.1993@o2.pl>
2015-07-06 16:15:19 +0200

    Add Ray

---
+++ framework/Ray.h
@@ -0,0 +1,20 @@
+//
+//  Ray.h
+//  
+//
+//  Created by Katarzyna Jablonska on 26.06.15.
+//
+//
+
+#ifndef RAY_H
+#define RAY_H
+
+#include <glm/vec3.hpp>
+
+struct Ray
+{
+    glm::vec3 origin;
+    glm::vec3 direction;
+};
+
+#endif /* defined(RAY_H) */
